d3a4698a3499d7ea40da0ccfc4826f069ee4a24c
Michael <Michael.ginalick@gmail.com>
2015-08-02 14:02:45 -0500

    12 of 19 complete

--- contact.js
+++ contact.js
@@ -22,4 +22,42 @@ Contact.createContact = function(str){
 }
 
 
+
+Contact.loadContacts = function(done){
+		var jsonfile = require('jsonfile')
+		jsonfile.readFile('data.json', function(err, data) {
+  	return done(err, data)
+	})
+}
+
+
+Contact.saveContacts = function(contacts, done) {
+	var jsonfile = require('jsonfile')
+	//var contacts = [ { name: "John Smith", number: "603-123-9090" } ]
+
+	jsonfile.writeFile('data.json', contacts, function(err, data){
+		return done(err)
+	})
+}
+
+Contact.saveContact = function(contact, done) {
+	var self = this
+	this.loadContacts(function(err, contacts){
+		if (err) {return done(err)}
+		contacts.push(contact)
+		self.saveContacts(contacts, done)
+	})
+}
+
+
+Contact.findContacts = function(name, done) {
+	this.loadContacts(function(err, contacts){
+		if (err) {return done(err)}
+			var names = contacts.filter(function(contact){ return contact.name == name })
+				done(err, names)
+	})
+}
+
+
+
 module.exports = Contact
--- test.js
+++ test.js
@@ -13,3 +13,32 @@ console.log(number)
 
 var contact = Contact.createContact("John Smith,604-123-9090")
 console.log(contact)
+
+
+var Contact = require('./contact')
+
+Contact.loadContacts(function(err, data) {
+  console.log(data) // -> should print the data from your data.json file
+})
+
+var Contact = require('./contact')
+
+var contacts = [ { name: "John Smith", number: "603-123-9090" } ]
+Contact.saveContacts(contacts, function(err){
+	console.log('success')
+})
+
+
+var Contact = require('./contact')
+var contact = { name: "John Smith", number: "604-123-9090" }
+Contact.saveContact(contact, function(err) {
+  console.log('success')
+})
+
+var Contact = require('./contact')
+var contact = { name: "John Smith", number: "604-123-9090" }
+Contact.findContact(contact, function(err){
+	console.log('success')
+})
+
+
